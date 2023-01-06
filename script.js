20e1f98b6d9d5bca3c1502281f42a24765e0280c
Humbert Kellyan <kellyanhumbert@gmail.com>
2023-01-06 21:09:52 +0100

    First Commit

---
+++ script.js
@@ -0,0 +1,10 @@
+function heures() {
+    window.location.href = '../heures/main';
+}
+function stedelorraine() {
+    window.location = "https://www.societedelorraine.fr"
+}
+function sdl() {
+    window.location = 'https://www.sdl-humbert.fr/'
+}
+
